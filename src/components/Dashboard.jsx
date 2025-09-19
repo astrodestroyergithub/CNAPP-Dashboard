dabfc117290228c29f456721c6e53bfc3dd41187
Tamojit Roy <tamojitroy140301@gmail.com>
2025-09-19 21:02:44 +0530

    Removed unused import

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Widget from './Widget';
 import { setModalOpen, setActiveCategory } from '../store/dashboardSlice';
