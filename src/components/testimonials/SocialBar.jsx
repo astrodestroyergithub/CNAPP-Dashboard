2dd5495fe80f02dfbb9ccae9d6cf63389cbe53e1
Tamojit Roy <tamojitroy140301@gmail.com>
2025-10-26 19:05:58 +0530

    Removed unused import

--- src/components/testimonials/SocialBar.jsx
+++ src/components/testimonials/SocialBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight, faWind, faCopy } from '@fortawesome/free-solid-svg-icons'
